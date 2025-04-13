32081357f98a912fb784da954c20ba233b99adf9
tourze-ci <you@example.com>
2025-04-13 12:36:19 +0000

    feat: 添加 json-rpc-async-bundle 包

---
+++ src/Attribute/AsyncExecute.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tourze\JsonRPCAsyncBundle\Attribute;
+
+#[\Attribute(\Attribute::TARGET_CLASS)]
+class AsyncExecute
+{
+}
---
+++ src/JsonRPCAsyncBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Tourze\JsonRPCAsyncBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class JsonRPCAsyncBundle extends Bundle
+{
+}
