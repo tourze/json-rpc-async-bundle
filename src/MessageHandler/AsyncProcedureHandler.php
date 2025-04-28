99fe335a168b2931846c6bd2917cc980b14a3e31
github-ci <you@example.com>
2025-04-28 08:33:02 +0000

    fix: 增加异常处理并更新依赖

--- src/MessageHandler/AsyncProcedureHandler.php
+++ src/MessageHandler/AsyncProcedureHandler.php
@@ -7,8 +7,11 @@ use Psr\Log\LoggerInterface;
 use Psr\SimpleCache\CacheInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 use Tourze\JsonRPC\Core\Contracts\EndpointInterface;
+use Tourze\JsonRPC\Core\Exception\JsonRpcException;
+use Tourze\JsonRPC\Core\Model\JsonRpcResponse;
 use Tourze\JsonRPCAsyncBundle\Entity\AsyncResult;
 use Tourze\JsonRPCAsyncBundle\Message\AsyncProcedureMessage;
+use Tourze\JsonRPCEndpointBundle\Serialization\JsonRpcResponseNormalizer;
 
 #[AsMessageHandler]
 class AsyncProcedureHandler
@@ -18,18 +21,32 @@ class AsyncProcedureHandler
         private readonly EntityManagerInterface $entityManager,
         private readonly CacheInterface $cache,
         private readonly LoggerInterface $logger,
+        private readonly JsonRpcResponseNormalizer $responseNormalizer,
     ) {
     }
 
     public function __invoke(AsyncProcedureMessage $message): void
     {
         $payload = $message->getPayload();
-        $content = $this->sdkEndpoint->index($payload);
-        $content = json_decode($content, true);
+
+        try {
+            $response = $this->sdkEndpoint->index($payload);
+            $response = json_decode($response, true);
+        } catch (\Throwable $exception) {
+            $this->logger->error('异步执行时发生未知异常', [
+                'taskId' => $message->getTaskId(),
+                'exception' => $exception,
+            ]);
+
+            $j = new JsonRpcResponse();
+            $j->setId($message->getTaskId());
+            $j->setError(new JsonRpcException(-1, $exception->getMessage(), previous: $exception));
+            $response = $this->responseNormalizer->normalize($j);
+        }
 
         try {
             // 先把结果写缓存，以更早地读取到
-            $this->cache->set(AsyncResult::CACHE_PREFIX . $message->getTaskId(), $content);
+            $this->cache->set(AsyncResult::CACHE_PREFIX . $message->getTaskId(), $response);
         } catch (\Throwable $exception) {
             $this->logger->error('提前写异步结果到缓存时失败', [
                 'exception' => $exception,
@@ -39,7 +56,7 @@ class AsyncProcedureHandler
 
         $record = new AsyncResult();
         $record->setTaskId($message->getTaskId());
-        $record->setResult($content);
+        $record->setResult($response);
         $this->entityManager->persist($record);
         $this->entityManager->flush();
     }
